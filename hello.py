92121aa33fb1b37064c5ea159b0c1320e266d12c
Steve Jensen <stevejensen@Steves-MBP.hsd1.co.comcast.net>
2017-08-08 18:59:07 -0600

    Update Chapter3 2

--- hello.py
+++ hello.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import make_response
+from flask import Flask, render_template
 from flask_script import Manager
 
 app = Flask(__name__)
@@ -8,14 +7,11 @@ manager = Manager(app)
 
 @app.route('/')
 def index():
-    response = make_response('<h1>This document carries a cookie!</h1>')
-    response.set_cookie('answer', '42')
-    return response
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {0}!</h1>'.format(name)
+    return render_template('user.html', name=name)
 
 if __name__ == '__main__':
     manager.run()
-    
